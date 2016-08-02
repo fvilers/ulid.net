1b0575cb61b6c2cb1d03c8558c1b515544505326
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-02 08:18:08 +0200

    Added EncodedTime implementation

--- Src/UlidNet/Ulid.cs
+++ Src/UlidNet/Ulid.cs
@@ -4,6 +4,9 @@ namespace UlidNet
 {
     public struct Ulid
     {
+        private const string Encoding = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+        private const int EncodingLength = 32;
+
         public static string NewUlid()
         {
             throw new NotImplementedException();
@@ -11,7 +14,17 @@ namespace UlidNet
 
         internal static string EncodeTime(long time, int length)
         {
-            throw new NotImplementedException();
+            var str = "";
+
+            for (var i = length; i > 0; i--)
+            {
+                var mod = (int)(time % EncodingLength);
+
+                str = Encoding[mod] + str;
+                time = (time - mod) / EncodingLength;
+            }
+
+            return str;
         }
 
         internal static string EncodeRandom(int length)
